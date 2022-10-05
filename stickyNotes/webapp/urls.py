8bfecd28a82d3e2d6684b407c6b19cbd91f56af3
Spencer <contact@vulntracker.app>
2022-10-05 13:15:47 -0400

    Add Authentication system

--- stickyNotes/webapp/urls.py
+++ stickyNotes/webapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='Index'),
+    path('register', views.register_request, name='Register'),
+    path('login', views.login_request, name='Login'),
+    path('logout', views.logout_request, name='Logout'),
 ]
